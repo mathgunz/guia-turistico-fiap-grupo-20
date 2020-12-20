ca8f133a663b6e2778b1826387fd30e6deadd877
mathgunz@gmail.com <mathgunz@gmail.com>
2020-12-20 10:42:36 -0300

    coisa nova

--- cliente/src/main/java/com/cliente/repositories/entities/ClienteMapper.java
+++ cliente/src/main/java/com/cliente/repositories/entities/ClienteMapper.java
@@ -6,13 +6,13 @@ public class ClienteMapper implements BaseMapper<ClienteEntity, Cliente> {
 
     @Override
     public Cliente toEntity(ClienteEntity entity) {
-        return new Cliente(
-                entity.getId(),
-                entity.getNome(),
-                entity.getCpf(),
-                entity.getTelefone(),
-                entity.getEmail(),
-                entity.getEndereco() != null ? new EnderecoMapper().toEntity(entity.getEndereco()) : null
-        );
+        return Cliente.newBuilder()
+                .setId(entity.getId())
+                .setCpf(entity.getCpf())
+                .setEmail(entity.getEmail())
+                .setEndereco(entity.getEndereco() != null ? new EnderecoMapper().toEntity(entity.getEndereco()) : null)
+                .setNome(entity.getNome())
+                .setTelefone(entity.getTelefone())
+                .build();
     }
 }
--- cliente/src/main/java/com/cliente/services/dtos/Cliente.java
+++ cliente/src/main/java/com/cliente/services/dtos/Cliente.java
@@ -10,60 +10,96 @@ public class Cliente implements Serializable {
     private String email;
     private Endereco endereco;
 
-    public Cliente(Long id, String nome, String cpf, Long telefone, String email, Endereco endereco) {
-        this.id = id;
-        this.nome = nome;
-        this.cpf = cpf;
-        this.telefone = telefone;
-        this.email = email;
-        this.endereco = endereco;
+    private Cliente(Builder builder) {
+        this.id = builder.id;
+        this.nome = builder.nome;
+        this.cpf = builder.cpf;
+        this.telefone = builder.telefone;
+        this.email = builder.email;
+        this.endereco = builder.endereco;
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private Long id;
+        private String nome;
+        private String cpf;
+        private Long telefone;
+        private String email;
+        private Endereco endereco;
+
+        private Builder() {
+        }
+
+        public Builder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setNome(String nome) {
+            this.nome = nome;
+            return this;
+        }
+
+        public Builder setCpf(String cpf) {
+            this.cpf = cpf;
+            return this;
+        }
+
+        public Builder setTelefone(Long telefone) {
+            this.telefone = telefone;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setEndereco(Endereco endereco) {
+            this.endereco = endereco;
+            return this;
+        }
+
+        public Builder of(Cliente cliente) {
+            this.id = cliente.id;
+            this.nome = cliente.nome;
+            this.cpf = cliente.cpf;
+            this.telefone = cliente.telefone;
+            this.email = cliente.email;
+            this.endereco = cliente.endereco;
+            return this;
+        }
+
+        public Cliente build() {
+            return new Cliente(this);
+        }
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getNome() {
         return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public String getCpf() {
         return cpf;
     }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
     public Long getTelefone() {
         return telefone;
-    }
-
-    public void setTelefone(Long telefone) {
-        this.telefone = telefone;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public Endereco getEndereco() {
         return endereco;
-    }
-
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
     }
 }
